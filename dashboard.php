cebf035b17ae173cc0918ae9ea5e05d65fa7e741
Bandara Samarakoon <bandarasamarakoon95@gmail.com>
2026-05-26 12:37:37 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -46,7 +46,7 @@ try {
     <title>My Dashboard - Cricket Arena</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="Assets/CSS/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     
