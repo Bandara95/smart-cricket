6df6383d885417cbff68e4075bbd7e1d05bee012
Bandara Samarakoon <bandarasamarakoon95@gmail.com>
2026-05-26 12:38:09 +0530

    Update login.php

--- login.php
+++ login.php
@@ -95,7 +95,7 @@ $_SESSION['admin_name'] = $user['full_name'];
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
-    <link rel="stylesheet" href="Assets/CSS/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
