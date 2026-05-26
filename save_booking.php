eed0f0f15fb9e3a30b0c01a5d85c8484a52381a0
Bandara Samarakoon <bandarasamarakoon95@gmail.com>
2026-05-26 12:39:13 +0530

    Update save_booking.php

--- save_booking.php
+++ save_booking.php
@@ -87,7 +87,7 @@ try {
     <title>Booking Success - Cricket Arena</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="Assets/CSS/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <meta http-equiv="refresh" content="3;url=dashboard.php">
 </head>
 <body>
