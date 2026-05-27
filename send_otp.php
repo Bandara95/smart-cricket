d9c8c859740df8566d3fa00480f8ecb80d27134d
Bandara Samarakoon <bandarasamarakoon95@gmail.com>
2026-05-27 09:17:15 +0530

    Update send_otp.php

--- send_otp.php
+++ send_otp.php
@@ -1,4 +1,6 @@
 <?php
+$is_secure = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https');
+session_set_cookie_params(['lifetime'=>0,'path'=>'/','secure'=>$is_secure,'httponly'=>true,'samesite'=>'Lax']);
 session_start();
 
 date_default_timezone_set('Asia/Colombo');
